492550bf219c2258332e84e1c888c434613ae195
nireee <nireee@outlook.com>
2021-05-04 20:52:22 -0700

    5.4 version
    single player, lack of artwork, have trouble moving the player after rolling the dice

---
+++ 168 group project/Assets/Scripts/Dice.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+    // Array of dice sides sprites to load from Resources folder
+    private Sprite[] diceSides;
+
+
+    // Reference to sprite renderer to change sprites
+    private SpriteRenderer rend;
+
+    public bool rolled = false;
+
+    private bool coroutineAllowed = true;
+
+    // Use this for initialization
+    public void Start () {
+
+        // Assign Renderer component
+        rend = GetComponent<SpriteRenderer>();
+
+        // Load dice sides sprites to array from DiceSides subfolder of Resources folder
+        diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+	}
+	
+    // If you left click over the dice then RollTheDice coroutine is started
+    public void OnMouseDown()
+    {
+        if(coroutineAllowed) StartCoroutine("RollTheDice");
+        
+    }
+
+    // Coroutine that rolls the dice
+    public IEnumerator RollTheDice()
+    {
+        coroutineAllowed = false;
+
+        // Variable to contain random dice side number.
+        // It needs to be assigned. Let it be 0 initially
+        int randomDiceSide = 0;
+
+        // Loop to switch dice sides ramdomly
+        // before final side appears. 20 itterations here.
+        for (int i = 0; i <= 20; i++)
+        {
+            // Pick up random value from 0 to 5 (All inclusive)
+            randomDiceSide = Random.Range(0, 5);
+
+            // Set sprite to upper face of dice from array according to random value
+            rend.sprite = diceSides[randomDiceSide];
+
+            // Pause before next itteration
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        // Assigning final side so you can use this value later in your game
+        // for player movement for example
+        PlayerControl.finalSide = randomDiceSide + 1;
+
+        PlayerControl.MovePlayer();
+
+        // Show final dice value in Console
+        Debug.Log(PlayerControl.finalSide);
+        coroutineAllowed = true;
+    }
+}
---
+++ 168 group project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public static GameObject player;
+    public static int player_start_index = 0;
+    public static int finalSide = 0;
+    public bool GameOver = false;
+    void Start()
+    {
+        player = GameObject.Find("player");
+        player.GetComponent<Colored>().move = false;
+    }
+    private void Update()
+    {
+        if (player.GetComponent<Colored>().area_index >
+            player_start_index + finalSide)
+        {
+            player.GetComponent<Colored>().move = false;
+            player_start_index = player.GetComponent<Colored>().area_index - 1;
+        }
+
+        if (player.GetComponent<Colored>().area_index >=
+            player.GetComponent<Colored>().areas.Length)
+        {
+            player.GetComponent<Colored>().area_index -= player.GetComponent<Colored>().areas.Length;
+        }
+        
+    }
+    public static void MovePlayer()
+    {
+        player.GetComponent<Colored>().move = true;
+    }
+}
